69e232fe1b6020c24a02b27fde07123ff868ccac
Nir Perets <nirperets@Nirs-MacBook-Pro.local>
2022-05-03 17:53:25 +0300

    fixes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const urlBuilder   = require('./Functions/getUrls')
 const fetchProduct = require('./Functions/fetchProducts');
 
 const app = express();
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
