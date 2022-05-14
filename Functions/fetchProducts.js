aaae156ff062adc9961c1adbbe04ba0b478d19d8
Nir Perets <nirperets@Nirs-MacBook-Pro.local>
2022-05-14 14:33:16 +0300

    fix

--- Functions/fetchProducts.js
+++ Functions/fetchProducts.js
@@ -67,6 +67,7 @@ const getProductImage = async (url) => {
     const options = { 
         headers: { 'User-Agent' : 'Chrome/62.0.3202.84' }
     }
+    console.log(url)
     await axios.get('https://${url}', options)
     .then(res => {
         console.log(res)
