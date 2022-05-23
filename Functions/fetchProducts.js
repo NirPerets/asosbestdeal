06adcfb36788da0e49945c998d5216a3427abadb
Nir Perets <nirperets@Nirs-MacBook-Pro.local>
2022-05-23 15:01:22 +0300

    asd

--- Functions/fetchProducts.js
+++ Functions/fetchProducts.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const res = require('express/lib/response');
 const fs = require('fs')
-const request = require('request')
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const fetchProduct = async (url) => {
     let product = {};
@@ -64,6 +64,24 @@ const fetchCountry = async (country, ils) => {
 }
 
 const getProductImage = async (url) => {
+    var xhr = new XMLHttpRequest()
+    xhr.open("GET", url)
+
+    xhr.setRequestHeader("Connection", "keep-alive");
+    xhr.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36");
+    xhr.setRequestHeader("Upgrade-Insecure-Requests", "1");
+    xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+    xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.9");
+    xhr.setRequestHeader("Accept-Encoding", "gzip, deflate");
+
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+    }};
+
+    xhr.send()
+    
     let product = {};
     const options = { 
         headers: { 
