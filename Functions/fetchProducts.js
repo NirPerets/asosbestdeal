d128a845eebb12e499167b6525ffbb3509223095
Nir Perets <nirperets@Nirs-MacBook-Pro.local>
2022-06-18 14:14:50 +0300

    news

--- Functions/fetchProducts.js
+++ Functions/fetchProducts.js
@@ -64,15 +64,31 @@ const fetchCountry = async (country, ils) => {
 
 const getProductImage = async (url) => {
     let product = {}
-    const options = {
+    let config = {
+        method: 'GET',
+        url: url,
         headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json'
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-language": "en-US,en;q=0.9",
+            "cache-control": "no-cache",
+            "pragma": "no-cache",
+            "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"101\", \"Google Chrome\";v=\"101\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
         }
-    }
+    };
 
-    await axios.get(url, options)
-    .then(res => console.log(res))
+    axios(config).then(async (response) => {
+        let data = response.data;
+        console.log(data);
+    }).catch((error) => {
+        console.log(error);
+    });
 
     return product
 }
