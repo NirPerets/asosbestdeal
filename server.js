2a79f9b617a7804194ec0378d3a7e32b5dc13ba4
Nir Perets <nirperets@Nirs-MacBook-Pro.local>
2023-03-27 17:43:03 +0300

    new updates

--- server.js
+++ server.js
@@ -48,9 +48,4 @@ cron.schedule('0 0 * * *', () => {
     setIlsPrice()
 })
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use(express.static(path.resolve(__dirname, "./client/build")));
